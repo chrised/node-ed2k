eb0253ff96d77a741d010089129a0fbf15e02c17
Chris Edwards <chrised@cisco.com>
2015-07-07 23:40:20 +0100

    Refactor to allow streams

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ var fs = require('fs'),
 
 var ED2K_CHUNK_SIZE = 9728000;
 
-/* stream - the file we're reading from.
+/* stream - the stream we're reading from.
  * hash - created and destroyed for each 9500KB chunk.
  * metahash - 9500KB chunks' binary md4 hashes are inserted until we expend
  * the stream.
  * hash_current_pos - used to measure the amount of data we've pumped into
  * the current hash.
  */
-var ed2k_hash_file = function(path, callback) {
-    var stream = fs.createReadStream(path);
+var ed2k_hash_stream = function(stream, callback) {
     var hash = crypto.createHash('md4');
     var metahash = crypto.createHash('md4');
     var hash_current_pos = 0;
@@ -46,34 +45,62 @@ var ed2k_hash_file = function(path, callback) {
     stream.on('end', function () {
         // Stream's over, insert the final hash, cb with the completed ed2k
         metahash.update(hash.digest('binary'));
-        callback(metahash.digest('hex'));
+        callback(null, metahash.digest('hex'));
     });
 };
 
-/* Turn the ed2k hash into an ed2k uri
+/* Turn the ed2k hash, filename and size into an ed2k uri
  */
-var ed2k_hash_to_uri = function(path, hash, cb) {
-    fs.stat(path, function (err, stat) {
-        if (err) {
-            cb(err);
+var ed2k_uri_for_hash = function(hash, filename, size) {
+    return('ed2k://|file|' + filename + '|' + size + '|' + hash);
+};
+
+/* Callback with an ed2k URI for a filepath (absolute or relative)
+ * path - Path to file. Type: String.
+ * callback - function(err, ed2k_uri)
+ *     ed2k_uri - an ed2k uri for the file at path. Type: String.
+ */
+var ed2k_uri_for_filepath = function(in_path, callback) {
+    var ehandle = function (err) {
+        if (err !== null && err !== undefined) {
+            callback(err);
+            return true;
+        }
+    };
+    fs.realpath(in_path, function (err, path) {
+        if (ehandle(err)) { return; }
+        fs.stat(path, function (err, stat) {
+            if (ehandle(err)) { return; }
+            ed2k_hash_stream(fs.createReadStream(path), function (err, hash) {
+                if (ehandle(err)) { return; }
+                callback(null, ed2k_uri_for_hash(hash, path.split('/').pop(), stat.size));
+            });
+        });
+    });
+};
+
+var ed2k_hash_for_stream = function(stream, callback) {
+    ed2k_hash_stream(stream, callback);
+};
+
+/* Callback with ed2k hash for provided filepath
+ * in_path - File to hash. Type: String
+ * callback - function(err, hash)
+ *      hash - ed2k has for the stream. Type: String
+ */
+var ed2k_hash_for_filepath = function(in_path, callback) {
+    fs.realpath(in_path, function (err, path) {
+        if (err !== null && err !== undefined) {
+            callback(err);
         } else {
-            cb('ed2k://|file|' + path.split('/').pop() + '|' + stat.size + '|' + hash);
+            ed2k_hash_stream(fs.createReadStream(path), callback);
         }
     });
 };
 
-/* The exposed function, takes an absolute filesystem path and a callback.
- * path - Absolute path to file. Type: String.
- * callback - function(ed2k_uri)
- *     ed2k_uri - an ed2k uri for the file at path. Type: String.
- */
-var ed2k_for_filepath = function(path, callback) {
-    if (typeof path === 'string') {
-        ed2k_hash_file(path, function (hash) {
-            ed2k_hash_to_uri(path, hash, callback);
-        });
-    }
-};
 
-module.exports.ed2k_for_filepath = ed2k_for_filepath;
+module.exports.ed2k_uri_for_filepath    = ed2k_uri_for_filepath;
+module.exports.ed2k_uri_for_hash        = ed2k_uri_for_hash;
+module.exports.ed2k_hash_for_filepath   = ed2k_hash_for_filepath;
+module.exports.ed2k_hash_for_stream     = ed2k_hash_stream;
 
